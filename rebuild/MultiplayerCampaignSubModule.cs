5682aed65a3a8fe2d7ce0461d0d4d72cbf62d482
mohammadrezaRX <156219408+mohammadrezaRX@users.noreply.github.com>
2026-08-29 14:42:58 +0330

    Add rebuild MultiplayerCampaignSubModule source

---
+++ rebuild/MultiplayerCampaignSubModule.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+
+namespace MultiplayerCampaign
+{
+    // Rebuild workspace placeholder.
+    // The full corrected source will be synchronized here in the next commit.
+    public sealed class RebuildMarker
+    {
+        public const string Version = "rebuild";
+    }
+}
